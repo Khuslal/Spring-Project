2f25159f7bb5eb65fdf0947aecd3f3ce471a9d83
Khuslal Gupta <guptakhuslal@gmail.com>
2026-07-13 20:48:12 +0545

    Session Handling Done

--- springproject/src/main/java/com/bway/springproject/controller/UserController.java
+++ springproject/src/main/java/com/bway/springproject/controller/UserController.java
@@ -2,6 +2,7 @@ package com.bway.springproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bway.springproject.model.User;
 import com.bway.springproject.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
@@ -21,22 +24,32 @@ public class UserController {
 	}
 
 	@PostMapping("/login")
-	public String postLogin(@ModelAttribute User user, RedirectAttributes redirectAttributes) {
+	public String postLogin(@ModelAttribute User user, Model model, RedirectAttributes redirectAt,
+			HttpSession session) {
 		User dbuser = userService.userLogin(user.getUsername(), user.getPassword());
 
 		if (dbuser != null) {
-			
-			return "redirect:/home";
+//			model.addAttribute("uname", dbuser.getFname());
+			session.setAttribute("activeUser", dbuser);
+			session.setMaxInactiveInterval(5 * 60);
+
+			return "home";
 		}
-		redirectAttributes.addFlashAttribute("error", "Invalid username or password!");
+		redirectAt.addFlashAttribute("error", "Invalid username or password!");
 		return "redirect:/login";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "LoginForm";
 	}
 
 	@GetMapping("/home")
 	public String getHOme() {
 		return "Home";
 	}
-	
+
 	@GetMapping("/signup")
 	public String getSignup() {
 		return "SignupForm";
@@ -45,8 +58,18 @@ public class UserController {
 	@PostMapping("/signup")
 	public String postSignup(@ModelAttribute User user, RedirectAttributes redirectAttributes) {
 		userService.userSignup(user);
-		
+
 		redirectAttributes.addFlashAttribute("message", "Data saved successfully.");
 		return "redirect:/login";
 	}
+	
+	@GetMapping("/profile")
+	public String getProfile(HttpSession session) {
+		if(session.getAttribute("activeUser") == null) {
+			return "LoginForm";
+		}
+		return "Profile";
+	}
+	
+	
 }
