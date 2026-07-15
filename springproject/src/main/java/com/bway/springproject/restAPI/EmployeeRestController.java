ad0acf34dcaefb5db941394a879eae711465b203
Khuslal Gupta <guptakhuslal@gmail.com>
2026-07-15 19:15:58 +0545

    creating rest api for get and post mapping

---
+++ springproject/src/main/java/com/bway/springproject/restAPI/EmployeeRestController.java
@@ -0,0 +1,50 @@
+package com.bway.springproject.restAPI;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bway.springproject.model.Employee;
+import com.bway.springproject.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeRestController {
+	
+	@Autowired
+	private EmployeeService empService;
+	
+	// Get All Employees Data
+	@GetMapping("/list")
+	public List<Employee> list() {
+		return empService.getAllEmps();
+	}
+	
+	// Get a particular employee's data 
+	@GetMapping("/{id}")
+	public Employee getEmployee(@PathVariable long id) {
+		return empService.getEmpById(4);
+	}
+	
+	@PostMapping("/add")
+	public String add(@RequestBody Employee emp) {
+		empService.addEmp(emp);
+		return "Data Added Successfully";
+	}
+	
+	@PostMapping("/add/many")
+	public String add(@RequestBody List<Employee> emps) {
+	    // Loop through the list and add each employee
+	    for (Employee emp : emps) {
+	        empService.addEmp(emp);
+	    }
+	    return "All Data Added Successfully";
+	}
+
+}
